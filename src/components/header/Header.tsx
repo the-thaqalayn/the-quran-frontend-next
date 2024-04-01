ab1704d6aa4401b46f8ffd36744cfd47b3535f79
Mehradi <mehradi.info@gmail.com>
2024-04-01 16:25:35 +0200

    Remove Font Awesom from project

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,25 +2,8 @@
 
 import React from "react";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import {
-  faEllipsisVertical,
-  faGear,
-  faHeart,
-  faMoon,
-  faSun,
-} from "@fortawesome/free-solid-svg-icons";
-
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-  const menuItems = [
-    { title: "Home", href: "#", ShowMainMenu: true },
-    { title: "Sponsor", href: "#", ShowMainMenu: false },
-    { title: "Feedback", href: "#", ShowMainMenu: true },
-    { title: "Developers", href: "#", ShowMainMenu: false },
-  ];
 
   return <div>header</div>;
 };
