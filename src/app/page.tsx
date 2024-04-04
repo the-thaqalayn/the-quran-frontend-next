d80099127db20c7d62b0cd3c619dd66a6eec7476
Mehradi <mehradi.info@gmail.com>
2024-04-05 00:20:23 +0200

    Adding Surah page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use server";
 
 import SurahList from "@/components/surah/SurahList";
-import { fetchChapters } from "@/db/queries";
+import { fetchChapters } from "@/db/queries/chapers";
 
 const Home = async ({}) => {
   return (
-    <div>
+    <div className="my-8">
       <SurahList fechData={fetchChapters} />
     </div>
   );
