7e2fdf6fef04e7cd88daf27f8190efca45ab7bf1
Mehradi <mehradi.info@gmail.com>
2024-04-06 18:54:52 +0200

    Adding fontFaceHelper for get page's font-face

---
+++ src/app/utils/fontFaceHelper.ts
@@ -0,0 +1,29 @@
+export const getFontFaceNameForPage = (pageNumber: number) => {
+  return `p${pageNumber}`;
+};
+
+export const getFontFaceSource = (pageNumber: number, isV2: boolean) => {
+  return `url('${process.env.PUBLIC_URL}/assets/fonts/hafs/${
+    isV2 ? "v2" : "v1"
+  }/woff2/p${pageNumber}.woff2') format('woff2') ,url('${
+    process.env.PUBLIC_URL
+  }/assets/fonts/hafs/${
+    isV2 ? "v2" : "v1"
+  }/woff/p${pageNumber}.woff') format('woff'), url('${
+    process.env.PUBLIC_URL
+  }/assets/fonts/hafs/${
+    isV2 ? "v2" : "v1"
+  }/ttf/p${pageNumber}.ttf') format('truetype')`;
+};
+
+export const getFontFace = (pageNumber: number, isV2: boolean) => {
+  return new FontFace(
+    getFontFaceNameForPage(pageNumber),
+    getFontFaceSource(pageNumber, isV2)
+  );
+};
+
+export const loadFontFace = async (fontFace: FontFace) => {
+  const loadedFont = await fontFace.load();
+  document.fonts.add(loadedFont);
+};
