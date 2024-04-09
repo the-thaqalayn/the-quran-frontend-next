8c43a954e0b6e9c64d52e816897de6ef37b40c5d
Mehradi <mehradi.info@gmail.com>
2024-04-09 12:25:39 +0200

    Adding pagination and cheange getVersesByChapter() function

--- src/db/queries/verses.ts
+++ src/db/queries/verses.ts
@@ -1,8 +1,40 @@
 import type { Verse } from "@prisma/client";
 import { db } from "@/db";
 
-export const getVersesByChapter = (chapterId: number): Promise<Verse[]> => {
+export const getVersesByPage = (v: Partial<Verse>): Promise<Verse[]> => {
   return db.verse.findMany({
-    where: { chapter_id: chapterId },
+    where: {
+      AND: [
+        {
+          ...(v.juz_number ? { juz_number: v.juz_number } : {}),
+        },
+        {
+          ...(v.hizb_number ? { hizb_number: v.hizb_number } : {}),
+        },
+        {
+          ...(v.rub_el_hizb_number
+            ? { rub_el_hizb_number: v.rub_el_hizb_number }
+            : {}),
+        },
+        {
+          ...(v.ruku_number ? { ruku_number: v.ruku_number } : {}),
+        },
+        {
+          ...(v.manzil_number ? { manzil_number: v.manzil_number } : {}),
+        },
+        {
+          ...(v.chapter_id ? { chapter_id: v.chapter_id } : {}),
+        },
+        {
+          ...(v.page_number ? { page_number: v.page_number } : {}),
+        },
+        {
+          ...(v.verse_key ? { verse_key: v.verse_key } : {}),
+        },
+      ],
+    },
+    orderBy: {
+      id: "asc",
+    },
   });
 };
