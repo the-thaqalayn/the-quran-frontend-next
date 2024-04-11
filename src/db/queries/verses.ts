fd33cdbdcd7e65d517c309eb5178f3b24ef31787
Mehradi <mehradi.info@gmail.com>
2024-04-11 22:42:20 +0200

    Adding Prisma's query for feching [min,max] of pages

--- src/db/queries/verses.ts
+++ src/db/queries/verses.ts
@@ -1,5 +1,6 @@
 import type { Verse } from "@prisma/client";
 import { db } from "@/db";
+import { select } from "@nextui-org/react";
 
 export const getVersesByPage = (v: Partial<Verse>): Promise<Verse[]> => {
   return db.verse.findMany({
@@ -37,4 +38,48 @@ export const getVersesByPage = (v: Partial<Verse>): Promise<Verse[]> => {
       id: "asc",
     },
   });
+};
+
+export const getMinMaxPage = async (
+  v: Partial<Verse>
+): Promise<[number, number]> => {
+  const aggregate = await db.verse.aggregate({
+    where: {
+      AND: [
+        {
+          ...(v.juz_number ? { juz_number: v.juz_number } : {}),
+        },
+        {
+          ...(v.hizb_number ? { hizb_number: v.hizb_number } : {}),
+        },
+        {
+          ...(v.rub_el_hizb_number
+            ? { rub_el_hizb_number: v.rub_el_hizb_number }
+            : {}),
+        },
+        {
+          ...(v.ruku_number ? { ruku_number: v.ruku_number } : {}),
+        },
+        {
+          ...(v.manzil_number ? { manzil_number: v.manzil_number } : {}),
+        },
+        {
+          ...(v.chapter_id ? { chapter_id: v.chapter_id } : {}),
+        },
+        {
+          ...(v.page_number ? { page_number: v.page_number } : {}),
+        },
+        {
+          ...(v.verse_key ? { verse_key: v.verse_key } : {}),
+        },
+      ],
+    },
+    _min: {
+      page_number: true,
+    },
+    _max: {
+      page_number: true,
+    },
+  });
+  return [aggregate._min.page_number ?? 0, aggregate._max.page_number ?? 0];
 };
