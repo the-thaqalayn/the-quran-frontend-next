c74af6e24fdd770095c1dc57bcc66458a9b9ac9b
Mehradi <mehradi.info@gmail.com>
2024-04-19 21:37:24 +0200

    Adding Language Schema in Prisma with seed

--- src/components/chapter/ChapterListCard.tsx
+++ src/components/chapter/ChapterListCard.tsx
@@ -19,7 +19,7 @@ const ChapterListCard = ({
       <div className="flex bg-white dark:bg-gray-600  group-hover:border-cyan-600 dark:group-hover:border-gray-50 group-hover:bg-gray-100 dark:group-hover:bg-sky-900  w-full  flex-row align-middle justify-between border border-solid border-gray-200 rounded cursor-pointer p-4">
         <div className="flex flex-row align-middle">
           <div className="flex rounded  group-hover:bg-cyan-600 group-hover:text-white bg-gray-100 dark:bg-cyan-800 align-middle justify-center rotate-45 border border-solid round me-5  w-10 h-10">
-            <span className="-rotate-45 dark:bg- my-auto font-bold align-baseline leading-5 justify-center m-0 p-0">
+            <span className="-rotate-45 my-auto font-bold align-baseline leading-5 justify-center m-0 p-0">
               {id}
             </span>
           </div>
