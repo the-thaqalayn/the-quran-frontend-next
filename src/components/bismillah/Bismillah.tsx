dc36b0daed74846a4f68268b815f0ffd44c87d0c
Mehradi <mehradi.info@gmail.com>
2024-05-11 14:42:44 +0200

    Fix error: updating text and fons the Bismillah component

--- src/components/bismillah/Bismillah.tsx
+++ src/components/bismillah/Bismillah.tsx
@@ -2,24 +2,32 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import styled from "styled-components";
 
 interface BismillahProps {
   chapterId?: number;
 }
-
+const TextArabic = styled.p`
+  line-height: 4rem;
+`;
 const Bismillah = ({ chapterId }: BismillahProps) => {
   const [cwb] = useState([1, 9]);
   const renderdBismillah =
     chapterId !== undefined && !cwb.includes(chapterId) ? (
       <div className="flex pt-5 pb-2 justify-center">
-        <Image
+        {/* <Image
           src="/assets/images/bismillah.svg"
           alt="bismillah"
           className="dark:invert-[.8]"
           width={220}
           height={45}
           priority
-        />
+        /> */}
+        <div className="text-end text-4xl text-cyan-900 break-words">
+          <TextArabic className="dark:text-cyan-500 font-textArabic">
+            بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ
+          </TextArabic>
+        </div>
       </div>
     ) : null;
   return renderdBismillah;
